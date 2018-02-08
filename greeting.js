7af4243d49bbcc28faa0c8ecbe5ee347a4ca278a
Ahmed <final.option@hotmail.com>
2018-02-08 11:14:44 +0200

    adding framework functionality

--- greeting.js
+++ greeting.js
@@ -5,7 +5,38 @@
 		return new Greeter.init(firstname, lastname, language);
 	}
 
-	Greeter.prototype = {};
+	/*I can creat a function to declare and expose these variable globaly and use
+	arguments keyword to access the passed parameters "this will increase readabilty,
+	reusabilty, maintainability and scalability.
+	*/
+	var supportedLanguages = ['en', 'es'];
+
+	var greetings = {
+		en: 'Hello',
+		es: 'Hola'
+	};
+
+	var formalreetings = {
+		en: 'Greetings',
+		es: 'Saludos'
+	};
+
+	var logMessages = {
+		en: 'Logged in',
+		es: 'Inicio sesion'
+	};
+
+	Greeter.prototype = {
+		fullname = function(){
+			return this.firstname + ' ' + this.lastname;
+		},
+
+		validate = function(){
+			if (supportedLanguages.indexOf(this.language) === -1){
+				throw this.language +' is not supported !';
+			}
+		},
+	};
 
 	Greeter.init(firstname, lastname, language){
 
@@ -14,7 +45,9 @@
 		self.lastname = lastname || "";
 		self.language = language || "en";
 	}
-
+	
 	Greeter.init.prototype = Greeter.prototype;
+
+	global.Greeter = global.G$ = Greeter;
 
 }(window, jquery));
